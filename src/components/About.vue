e8116678e73434840f59dfc0296fa915994215ce
Rushikesh Kumbhar <36808434+rushikeshkumbhar@users.noreply.github.com>
2018-03-17 03:44:18 -0700

    initial

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+      <h3>Hello {{their_name}}, this about page</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  data() {
+    return {
+      their_name:this.$route.params.name
+    }
+  },
+}
+</script>
